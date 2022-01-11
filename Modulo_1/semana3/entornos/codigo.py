de66ab90512337da7ea1552257ad0d730b344f76
jesus.gutierrez.lopez <jesus271088@gmail.com>
2022-01-10 20:18:00 -0600

    ajuste a las carpetas

---
+++ Modulo_1/semana3/entornos/codigo.py
@@ -0,0 +1,5 @@
+import numpy as np
+import pandas as pd
+
+dates = pd.date_range("20130101", periods=6)
+print(dates)
---
+++ Modulo_1/semana3/entornos/main.py
@@ -0,0 +1,7 @@
+from  operaciones.basicas import suma as sum
+from  operaciones.basicas import resta as rest
+from  operaciones.basicas import multiplicacion as mult
+
+print(sum(10,10))
+print(rest(10,10))
+print(mult(10,10))
