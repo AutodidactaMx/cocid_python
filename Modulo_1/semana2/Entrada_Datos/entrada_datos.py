910ee38c4309e060d38fb6da6f114d70409c56b1
jesus.gutierrez.lopez <jesus271088@gmail.com>
2022-01-27 21:13:37 -0600

    ENTRADA DE DATOS

---
+++ Modulo_1/semana2/Entrada_Datos/entrada_datos.py
@@ -0,0 +1,5 @@
+# Demostración de entrada de datos.
+nombre = input("Buen día me podía decir su nombre por favor : ")
+
+print("Bienvenido al sistema ",nombre," Gracias por usar mi programa.",  sep="'")
+
---
+++ Modulo_1/semana2/Entrada_Datos/entrada_datos_convertidos.py
@@ -0,0 +1,8 @@
+# Demostración de entrada de datos.
+nombre = input("Buen día me podía decir su nombre por favor : ")
+edad = int(input("Puede proporcionarnos su edad en años por favor : "))
+altura = float(input("Puede proporcionarnos su altura en metros por favor : "))
+
+print("Bienvenido al sistema ",nombre," con ", edad,
+      " años de edad y ", altura, " metros ",  sep="'")
+print("Gracias por usar mi programa.")
