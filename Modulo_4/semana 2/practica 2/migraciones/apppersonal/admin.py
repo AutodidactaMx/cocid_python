0cdd0d833d753dbadb2bd9c31ac4febb86b25600
jesus.gutierrez.lopez <jesus271088@gmail.com>
2022-05-08 01:04:43 -0500

    migracion y sitio

---
+++ Modulo_4/semana 2/practica 2/migraciones/apppersonal/admin.py
@@ -0,0 +1,33 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models  import Persona,TipoEmpleado
+
+class PersonaAdmin(admin.ModelAdmin):
+    fieldsets = (        
+        ('Informacion basica', {
+            'fields': ('rfc','nombre_completo')
+        }),
+        ('Datos generales', {
+            'fields': ('direccion','tipo_empleado','es_empleado','edad','altura','cintura')
+        }),
+        ('Datos empresa', {
+            'fields': ('fecha_registro','sueldo')
+        }),
+    )    
+    list_display = ('rfc','nombre_completo','direccion')
+    list_display_links = ('rfc','nombre_completo')
+    list_editable = ('direccion',)
+    list_filter = ('nombre_completo',)
+    search_fields = ('rfc',)
+class TipoEmpleadoAdmin(admin.ModelAdmin):
+    
+    fields = ['descripcion']
+    list_display = ('id','descripcion')
+    list_display_links = ('id',)
+    list_editable = ('descripcion',)
+    list_filter = ('descripcion',)
+    search_fields = ('descripcion',)
+    
+admin.site.register(Persona,PersonaAdmin)
+admin.site.register(TipoEmpleado,TipoEmpleadoAdmin)
