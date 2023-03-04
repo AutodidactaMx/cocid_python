e42dffa2d087741236d44b70540817e557f89a22
jgutierrezhologram <jgutierrez@hologram-brain.com>
2023-03-03 23:34:53 -0600

    s

--- Modulo_2/semana1/figuras_geometricas/main.py
+++ Modulo_2/semana1/figuras_geometricas/main.py
@@ -1,5 +1,4 @@
-from circulo import Circulo
-from cuadrado import Cuadrado
+
 from triangulo_equilatero import Triangulo_Equilatero
 from triangulo_isosceles import Triangulo_Isosceles
 print("Trinagulo isosceles")
@@ -10,11 +9,3 @@ print("Trinagulo equilatero")
 triangular_equilatero = Triangulo_Equilatero(10)
 print(triangular_equilatero.perimetro())
 print(triangular_equilatero.area())
-print("Cuadrado")
-cuadrado = Cuadrado(5)
-print(cuadrado.area())
-print(cuadrado.perimetro())
-print("Circulo")
-circulo = Circulo(5)
-print(circulo.area())
-print(circulo.perimetro())
