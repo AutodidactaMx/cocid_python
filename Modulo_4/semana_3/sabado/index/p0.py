a97d4a7331552ab71a3d8369a0ae96bf5679a533
jgutierrezhologram <jgutierrez@hologram-brain.com>
2023-05-26 23:39:19 -0600

    ..

---
+++ Modulo_4/semana_3/sabado/index/p0.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Crear DataFrame de ejemplo
+data = {'nombre': ['Juan', 'María', 'Pedro', 'Ana', 'Luis'],
+        'género': ['Masculino', 'Femenino', 'Masculino', 'Femenino', 'Masculino']}
+df = pd.DataFrame(data)
+
+# Contar el número de ocurrencias de cada categoría
+count = df['género'].value_counts()
+
+# Crear gráfico de barras
+plt.bar(count.index, count.values)
+
+# Personalizar el gráfico
+plt.title('Distribución de Género')
+plt.xlabel('Género')
+plt.ylabel('Cantidad')
+
+# Mostrar el gráfico
+plt.show()
