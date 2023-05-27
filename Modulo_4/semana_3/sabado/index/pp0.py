43ee765a4123b9e8e52d180adc0d1d1113d61cb8
jgutierrezhologram <jgutierrez@hologram-brain.com>
2023-05-27 09:42:40 -0600

    .s

--- Modulo_4/semana_3/sabado/index/pp0.py
+++ Modulo_4/semana_3/sabado/index/pp0.py
@@ -5,7 +5,8 @@ import matplotlib.pyplot as plt
 data = {'nombre': ['Juan', 'María', 'Pedro', 'Ana', 'Luis'],
         'género': ['Masculino', 'Femenino', 'Masculino', 'Femenino', 'Masculino']}
 df = pd.DataFrame(data)
-
+df['género'] = pd.Categorical(df['género'].apply(str))
+print(df.info())
 # Contar el número de ocurrencias de cada categoría
 count = df['género'].value_counts()
 
